a2ae433a6ef9ff007d1e510c4204739abd73ff66
Riya Agrahari <122736924+riu2028@users.noreply.github.com>
2023-11-06 13:58:51 +0530

    Create contact_form.php

---
+++ contact_form.php
@@ -0,0 +1,33 @@
+<!doctype html>
+  <html>
+      <head>
+        <title>Contact Form</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      </head>
+        <body>
+            <div>
+              <div class="contact-form-wrapper d-flex justify-content-center">
+                    <form action="#" class="contact-form">
+                        <h5 class="title">Contact us</h5>
+                            <p class="description">Feel free to contact us if you need any assistance, any help or another question.</p>
+                                <div>
+                                    <input type="text" class="form-control rounded border-white mb-3 form-input" id="name" placeholder="Name" required>
+                                </div>
+                                <div>
+                                    <input type="email" class="form-control rounded border-white mb-3 form-input" placeholder="Email" required>
+                                </div>
+                                <div>
+                                  <textarea id="message" class="form-control rounded border-white mb-3 form-text-area" rows="5" cols="30" placeholder="Message" required></textarea>
+                                </div>
+                                  <div class="submit-button-wrapper">
+                                      <input type="submit" value="Send">
+                                </div>
+                    </form>
+                </div>
+            </div>
+        </body>
+  </html>
